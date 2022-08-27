3e5792125d24e2849c71e00a5fed444b55a585ef
Xeviousbr <xeviousbr@gmail.com>
2022-08-26 21:28:26 -0300

    Inicial

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Relou Ourde',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Relou Ourde'),
@@ -39,13 +30,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'Este é um projeto inicial em Flutter:',
             ),
             Text(
-              'Ainda não tem nada',
+              'Ainda não tem nada!',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
