e8776d7874d61a9ed8461309ca8f13ce17ee42bd
Xeviousbr <xeviousbr@gmail.com>
2022-08-31 22:05:02 -0300

    Atualizei o nome do App

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('OT - Organizador de Tarefas'),
       ),
       body: Center(
         child: Column(
