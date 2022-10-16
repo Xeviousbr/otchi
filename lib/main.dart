71c25ec708e527496d62ea9cc47ff84fd4261000
Xeviousbr <xeviousbr@gmail.com>
2022-10-16 02:32:57 -0300

    Inicio do carregamento da lista

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:ot/login_page/login_page.dart';
 import 'cadastrar_tarefa.dart';
+import 'tarefa.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,6 +33,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var list = [
+    {'id': "1", "Nome": "Tarefa 1"},
+    {'id': "2", "date": "Tarefa 2"},
+    {'id': "3", "date": "Tarefa 3"}
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,45 +45,39 @@ class _MyHomePageState extends State<MyHomePage> {
           title: const Text('OT - Organizador de Tarefas'),
         ),
         body: Column(children: [
-          Center(
-              heightFactor: 2,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => CadastrarTarefa()),
-                  );
-                },
-                child: const Text('Nova Tarefa',
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      fontSize: 32,
-                      color: Colors.black,
-                    )),
-              )),
-          const Text('Tarefa 1',
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.black,
-              ))
+          Column(children: <Widget>[
+            Center(
+                heightFactor: 2,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CadastrarTarefa()),
+                    );
+                  },
+                  child: const Text('Nova Tarefa',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.black,
+                      )),
+                )),
+            Table(
+                border: TableBorder.all(color: Colors.black),
+                columnWidths: {},
+                children: [
+                  for (var item in list)
+                    TableRow(children: [
+                      Text("item.Nome",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                            fontSize: 32,
+                            color: Colors.black,
+                          ))
+                    ])
+                ])
+          ])
         ]));
   }
 }
-
-/* class Tarefas {
-  String? Nome;
-  int? Prioridade;
-  bool? HabDiaSem;
-  bool? HamSab;
-  bool? HabDom;
-  bool? Habilitado;
-
-  Tarefas(
-      {this.Nome,
-      this.Prioridade,
-      this.HabDiaSem,
-      this.HamSab,
-      this.HabDom,
-      this.Habilitado});
-} */
