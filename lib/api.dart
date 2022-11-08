415a58b608712129ab351eb469f5ff4e9f5f8525
Xeviousbr <xeviousbr@gmail.com>
2022-11-08 01:21:55 -0300

    Api para marcar o inicio

--- lib/api.dart
+++ lib/api.dart
@@ -8,7 +8,6 @@ class API {
   static Future cadastra(Tarefa tar) async {
     final dados =
         "idUsuario=${tar.idUser}&Nome=${tar.nome}&Prioridade=${tar.prioridade}&HoraLim=${tar.hora}&HabDiaSem=${tar.habDiaSem}&HabSab=${tar.habDom}&HabDom=${tar.habDom}&Habilitado=${tar.habilitado}";
-
     String? baseUrl = "https://tele-tudo.com/ot?op=2&$dados";
     var url = baseUrl;
     return await http.get(Uri.parse(url));
@@ -26,11 +25,22 @@ class API {
       return [];
     }
     final url = 'https://tele-tudo.com/ot?op=3&idUsuario=$userId';
-    // final url = 'https://635d4a9fcb6cf98e56b197ad.mockapi.io/tarefas';
-    //
     final response = await http.get(Uri.parse(url));
-    print(response.body);
     final tarefas = jsonDecode(response.body) as List<dynamic>;
     return tarefas.map((data) => TarLista.fromJson(data));
+    // JSon fake criado pelo Cristian
+    // final url = 'https://635d4a9fcb6cf98e56b197ad.mockapi.io/tarefas';
+  }
+
+  static Future deleta(String id) async {
+    String baseUrl = "https://tele-tudo.com/ot?op=4&id=$id";
+    var url = baseUrl;
+    return await http.get(Uri.parse(url));
+  }
+
+  static Future inicio(String id) async {
+    String baseUrl = "https://tele-tudo.com/ot?op=5&id=$id";
+    var url = baseUrl;
+    return await http.get(Uri.parse(url));
   }
 }
