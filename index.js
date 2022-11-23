deb187f6c1a40c01321488a8a7cb059771a436c5
Cristian Madrid <cristianmadridd@gmail.com>
2022-11-23 09:46:45 -0300

    feat: add oncreate user

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-exports.helloWorld = (req, res) => {
-    let message = req.query.message || req.body.message || "Hello World!";
-    res.status(200).send(message);
-};
+var admin = require("firebase-admin");
+var functions = require("firebase-functions");
+
+// Pass the Firebase config directly to initializeApp() to auto-configure
+// the Admin Node.js SDK.
+// admin.initializeApp(functions.config().firebase);
+
+exports.onCreateUser = functions.auth.user().onCreate((user) => {
+    // admin.firestore().collection('users').doc(user.data.uid).set({});
+});
