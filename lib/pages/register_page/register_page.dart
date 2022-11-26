32fab1f89ec7be8f6066aba2ae82c400d140f112
Cristian Madrid <cristianmadridd@gmail.com>
2022-11-26 17:00:51 -0300

    fix: redirect on register

--- lib/pages/register_page/register_page.dart
+++ lib/pages/register_page/register_page.dart
@@ -20,104 +20,106 @@ class _RegisterPageState extends State<RegisterPage> {
     final theme = Theme.of(context);
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 241, 241, 241),
-      body: BlocBuilder<ResgisterBloc, RegisterState>(
+      body: BlocListener(
         bloc: bloc,
-        builder: (context, state) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      "Faça seu cadastro",
-                      style: theme.textTheme.titleMedium,
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 35,
-                ),
-                TextField(
-                  onChanged: (text) {
-                    setState(() {
-                      email = text;
-                    });
-                  },
-                  style: theme.textTheme.bodyMedium,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    labelText: "Email:",
-                    suffixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder(),
+        listener: (_, state) {
+          if (state is RegisterSucceedState) {
+            Navigator.of(context).pop();
+          }
+        },
+        child: BlocBuilder<ResgisterBloc, RegisterState>(
+          bloc: bloc,
+          builder: (context, state) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        "Faça seu cadastro",
+                        style: theme.textTheme.titleMedium,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextField(
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  TextField(
                     onChanged: (text) {
                       setState(() {
-                        password = text;
+                        email = text;
                       });
                     },
                     style: theme.textTheme.bodyMedium,
-                    obscureText: isPasswordVisible,
-                    decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                        icon: isPasswordVisible
-                            ? const Icon(Icons.visibility_off)
-                            : const Icon(Icons.visibility),
-                        onPressed: () => setState(
-                            () => isPasswordVisible = !isPasswordVisible),
-                      ),
-                      labelText: "Senha:",
-                      border: const OutlineInputBorder(),
-                    )),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    padding: const EdgeInsets.all(15),
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      labelText: "Email:",
+                      suffixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                  onPressed: () {
-                    bloc.add(TryToRegisterEvent(email, password));
-                  },
-                  child: state is RegisterLoadingState
-                      ? const CircularProgressIndicator(
-                          color: Colors.white,
-                        )
-                      : Text(
-                          "Cadastrar",
-                          style: theme.textTheme.bodyLarge,
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                      onChanged: (text) {
+                        setState(() {
+                          password = text;
+                        });
+                      },
+                      style: theme.textTheme.bodyMedium,
+                      obscureText: isPasswordVisible,
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          icon: isPasswordVisible ? const Icon(Icons.visibility_off) : const Icon(Icons.visibility),
+                          onPressed: () => setState(() => isPasswordVisible = !isPasswordVisible),
                         ),
-                ),
-                const SizedBox(height: 16),
-                OutlinedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    padding: const EdgeInsets.all(15),
+                        labelText: "Senha:",
+                        border: const OutlineInputBorder(),
+                      )),
+                  const SizedBox(
+                    height: 20,
                   ),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text(
-                    "Cancelar",
-                    style: theme.textTheme.bodyLarge
-                        ?.copyWith(color: Colors.black),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      padding: const EdgeInsets.all(15),
+                    ),
+                    onPressed: () {
+                      bloc.add(TryToRegisterEvent(email, password));
+                    },
+                    child: state is RegisterLoadingState
+                        ? const CircularProgressIndicator(
+                            color: Colors.white,
+                          )
+                        : Text(
+                            "Cadastrar",
+                            style: theme.textTheme.bodyLarge,
+                          ),
                   ),
-                )
-              ],
-            ),
-          );
-        },
+                  const SizedBox(height: 16),
+                  OutlinedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      padding: const EdgeInsets.all(15),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      "Cancelar",
+                      style: theme.textTheme.bodyLarge?.copyWith(color: Colors.black),
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
