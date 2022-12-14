431e6f5278243d09d76575908b93ee363831dd1e
Xeviousbr <xeviousbr@gmail.com>
2022-12-14 00:44:11 -0300

    Atualiza a pagina, caso termine a tarefa
    Diminui o fator de 1.1 para 1.09

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -36,8 +36,8 @@ class API {
         .snapshots()
         .map((event) => event.docs.map((doc) => doc.data()))
         .map((items) => items.toList()
-          ..sort((a, b) => (a.tempo * (1 + ((a.prioridade - 1) * 1.1)))
-              .compareTo(b.tempo * (1 + ((b.prioridade - 1) * 1.1)))));
+          ..sort((a, b) => (a.tempo * (1 + ((a.prioridade - 1) * 1.09)))
+              .compareTo(b.tempo * (1 + ((b.prioridade - 1) * 1.09)))));
   }
 
   static Future deleta(String id) async {
