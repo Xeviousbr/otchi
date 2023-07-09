50fdd1c16936c9f72970a9714de32f2be7ef8fa5
Xeviousbr <xeviousbr@gmail.com>
2023-07-09 20:34:59 -0300

    add sentry

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'app.dart';
 import 'firebase_options.dart';
 
@@ -20,5 +21,14 @@ Future<void> main() async {
     return true;
   };
 
-  runApp(const App());
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://e17227a20a804b5e942495f61ac9aead@o4505502023221249.ingest.sentry.io/4505502028464128';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 100;
+    },
+    appRunner: () => runApp(const App()),
+  );
 }
