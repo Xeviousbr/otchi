0f712b09d8fc21d4a789118223ca3f241a310546
Xeviousbr <xeviousbr@gmail.com>
2024-03-14 00:21:48 -0300

    Correção do botão de cadastro

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ot/services/auth_service.dart';
-
 import 'components/theme.dart';
 import 'models/tarefa.dart';
 import 'pages/cadastrar_tarefa.dart';
-
 import 'pages/login/login_page.dart';
 import 'pages/register_page/register_page.dart';
 import 'pages/home/home_page.dart';
@@ -25,28 +23,26 @@ class App extends StatelessWidget {
               stream: AuthService.estaLogado(),
               builder: (context, snapshot) {
                 if (snapshot.data == null) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                if (!snapshot.data!) {
-                  return const LoginPage();
-                }
-                if (settings.name == '/home_page') return const HomePage();
-                if (settings.name == '/cadastrar_tarefa') {
-                  return CadastrarTarefa();
-                } // Remova o 'const' aqui
-                if (settings.name == '/editar_tarefa') {
-                  final tarefa = (ModalRoute.of(context)?.settings.arguments
-                      as Map<String, dynamic>?)?['tarefa'] as Tarefa?;
-                  return CadastrarTarefa(tarefa: tarefa);
+                  return const Center(child: CircularProgressIndicator());
                 }
                 if (settings.name == '/cadastro_user') {
                   return const RegisterPage();
                 }
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
+                if (!snapshot.data!) {
+                  return const LoginPage();
+                }
+                switch (settings.name) {
+                  case '/home_page':
+                    return const HomePage();
+                  case '/cadastrar_tarefa':
+                    return CadastrarTarefa();
+                  case '/editar_tarefa':
+                    final tarefa = (ModalRoute.of(context)?.settings.arguments
+                        as Map<String, dynamic>?)?['tarefa'] as Tarefa?;
+                    return CadastrarTarefa(tarefa: tarefa);
+                  default:
+                    return const Center(child: CircularProgressIndicator());
+                }
               },
             ),
           ),
